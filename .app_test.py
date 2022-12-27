c6eb26ac1d8ab11849ab8afcf8e538de42f97618
Mayke Alex Miranda Plaça <mayke.suporte+github@gmail.com>
2022-12-27 18:20:50 -0400

    Update .app_test.py

--- .app_test.py
+++ .app_test.py
@@ -1,4 +1,6 @@
 from simple_selenium import Simple_Selenium
 ss = Simple_Selenium()
 ss.driver.get('https://www.google.com')
+search_input = ss.fbt('input', False)
+search_input.send_keys('GOOGLE')
 print('\nfinished')
