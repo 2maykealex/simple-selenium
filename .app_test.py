64d44a71ce7ea235e79f475a13c3b68f30c71fee
Mayke Alex Miranda Plaça <mayke.suporte+github@gmail.com>
2022-12-28 11:56:22 -0400

    TESTS

--- .app_test.py
+++ .app_test.py
@@ -1,6 +1,17 @@
 from simple_selenium import Simple_Selenium
 ss = Simple_Selenium()
 ss.driver.get('https://www.google.com')
-search_input = ss.fbt('input', False)
-search_input.send_keys('GOOGLE')
+
+#TEST 1
+# search_input = ss.fbc('gLFyf', all=False)
+# search_input.send_keys('GOOGLE')
+
+#TEST 2
+search_input = ss.wait4element('gLFyf', 'class')
+if search_input:
+    search_input.send_keys('GOOGLE')
+    search_input = ss.wait4element('sbct', 'class')
+    if search_input:
+        print(search_input.text)
+
 print('\nfinished')
